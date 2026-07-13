0661eca5a0f083aa865f4ddfc99b217613e814b4
keerthibalaga123-collab <keerthibalaga123@gmail.com>
2026-07-13 14:56:34 +0530

    Added decoder2to4.v

---
+++ verilog/decoder2to4.v
@@ -0,0 +1,18 @@
+module decoder2to4(
+
+input A,
+input B,
+
+output D0,
+output D1,
+output D2,
+output D3
+
+);
+
+assign D0 = ~A & ~B;
+assign D1 = ~A & B;
+assign D2 = A & ~B;
+assign D3 = A & B;
+
+endmodule
