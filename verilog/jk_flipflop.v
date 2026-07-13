936b3e83f77fa1c38c12f0e0f001d9594982c8f0
keerthibalaga123-collab <keerthibalaga123@gmail.com>
2026-07-13 18:43:29 +0530

    Added jk_flipflop.v

---
+++ verilog/jk_flipflop.v
@@ -0,0 +1,18 @@
+module jk_flipflop(
+input J,
+input K,
+input CLK,
+output reg Q
+);
+
+always @(posedge CLK)
+begin
+case({J,K})
+2'b00: Q<=Q;
+2'b01: Q<=0;
+2'b10: Q<=1;
+2'b11: Q<=~Q;
+endcase
+end
+
+endmodule
